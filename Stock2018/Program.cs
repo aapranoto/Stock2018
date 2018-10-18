55aa4f5c88ef6b8c30f201f402b422c6ecaef4a6
Albertus <aapranoto@yahoo.com>
2018-10-18 14:13:15 -0700

    Initial commit

---
+++ Stock2018/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StockPick_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
